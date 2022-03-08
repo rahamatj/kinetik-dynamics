5c6ff57d2d656ce121f01f51e7cedba76a4f85a3
Rahamat Jahan <rahamatj92@gmail.com>
2022-03-08 23:02:05 +0600

    Set up Customer database

---
+++ app/Models/Customer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'address'
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+}
